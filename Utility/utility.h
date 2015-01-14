71227d974c6673244dea2973098bd754a87e3ce6
vpcola <vpcola@gmail.com>
2015-01-14 23:54:21 +0800

    Revert "Initial commit"
    This reverts commit 4c4fc8a5105e1c6a1478beb94532cbb50d0f2cd1.

--- Utility/utility.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UTILITY_H__
-#define __UTILITY_H__
-
-/* lightweight implementation of some string functions */
-char *_strtok(char *str, const char *delim, char **saveptr);
-int _stricmp (const char *p1, const char *p2);
-
-#endif
-
